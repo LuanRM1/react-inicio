c260b638ed6d309bb0a53151ca7ca145d48452c2
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2023-12-05 08:51:10 -0300

    Endpoint utilizando POST

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -13,48 +13,61 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map = () => {
-    const [points, setPoints] = useState([]);
-  
-    useEffect(() => {
-      // Função para chamar a API
-      const fetchPoints = async () => {
-        try {
-          const response = await fetch('http://localhost:3001/ativos');
-          const data = await response.json();
-  
-          // Atualiza os pontos um por um
-          data.forEach((point) => {
-            if (point.lat !== null && point.long !== null && point.lat !== 0 && point.long !== 0) {
-              setPoints((prevPoints) => [...prevPoints, point]);
-            }
-          });
-        } catch (error) {
-          console.error('Erro ao obter dados da API:', error);
-        }
-      };
-  
-      // Chama a função de busca quando o componente for montado
-      fetchPoints();
-    }, []); // O array vazio assegura que a função só será chamada uma vez, equivalente ao componentDidMount
-  
-    return (
-      <MapContainer
-        center={[-23.553950311165032, -46.73351577773466]}
-        zoom={6}
-        style={{ height: '100vh', width: '50%'}}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-  
-        {/* Adicione marcadores para cada ponto */}
-        {points.map((point) => (
-          <Marker key={point.id} position={[point.lat, point.long]}>
-            <Popup>{`ID: ${point.id} - Data: ${point.data}`}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    );
-  };
+  const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    // Função para chamar a API
+    const fetchPoints = async () => {
+      try {
+        // Definindo o corpo da requisição com o ID desejado
+        const requestBody = {
+          id: "1" // Substitua com o valor real do ID
+        };
+
+        // Realizando a chamada da API
+        const response = await fetch('http://localhost:3001/ativos', {
+          method: 'POST', // Método POST para enviar dados
+          headers: {
+            'Content-Type': 'application/json' // Especificando o formato do conteúdo
+          },
+          body: JSON.stringify(requestBody) // Convertendo o corpo da requisição para JSON
+        });
+
+        const data = await response.json();
+
+        // Atualiza os pontos um por um
+        data.forEach((point) => {
+          if (point.lat !== null && point.long !== null && point.lat !== 0 && point.long !== 0) {
+            setPoints((prevPoints) => [...prevPoints, point]);
+          }
+        });
+      } catch (error) {
+        console.error('Erro ao obter dados da API:', error);
+      }
+    };
+
+    // Chama a função de busca quando o componente for montado
+    fetchPoints();
+  }, []); // O array vazio assegura que a função só será chamada uma vez, equivalente ao componentDidMount
+
+  return (
+    <MapContainer
+      center={[-23.553950311165032, -46.73351577773466]}
+      zoom={6}
+      style={{ height: '100vh', width: '50%'}}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {/* Adicione marcadores para cada ponto */}
+      {points.map((point) => (
+        <Marker key={point.id} position={[point.lat, point.long]}>
+          <Popup>{`ID: ${point.id} - Data: ${point.data}`}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
   
   export default Map;
